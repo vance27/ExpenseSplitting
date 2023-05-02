7bab7460098f54e98ee5b690985d14b9c6e5c98b
Brian Vance <vance071@umn.edu>
2023-05-02 16:41:19 -0500

    some update with auth0 stuff

--- apps/mint-split-backend/src/main.ts
+++ apps/mint-split-backend/src/main.ts
@@ -4,25 +4,37 @@
  */
 
 import express from 'express';
-import * as path from 'path';
+import { join } from 'path';
+// import * as path from 'path';
 import * as cors from 'cors';
 
 const app = express();
 
 app.use(cors.default());
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+// app.use('/assets', express.static(path.join(__dirname, 'assets')));
+// Start Auth0
+app.use(express.static(join(__dirname, 'public')));
 
-app.get('/api', (_, res) => {
-    res.send({ message: 'Welcome to mint-split-backend!' });
+app.get('/auth_config.json', (_, res) => {
+    res.sendFile(join(__dirname, 'public', 'auth_config.json'));
 });
 
-app.post('/api/login', (_, res) => {
-    res.send({ token: '1234567890' });
+app.get('/', (_, res) => {
+    res.sendFile(join(__dirname, 'public', 'index.html'));
 });
+// end Auth0
+
+// app.get('/api', (_, res) => {
+//     res.send({ message: 'Welcome to mint-split-backend!' });
+// });
+
+// app.post('/api/login', (_, res) => {
+//     res.send({ token: '1234567890' });
+// });
 
 const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}/api`);
+    console.log(`Listening at http://localhost:${port}/`);
 });
 server.on('error', console.error);
