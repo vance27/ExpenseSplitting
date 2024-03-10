81e00736ad311d380c0ede93e286b20bb398d424
Brian Vance <briakvanc27@gmail.com>
2024-03-10 16:05:26 -0500

    update

--- apps/mint-split-next/src/pages/profile/index.tsx
+++ apps/mint-split-next/src/pages/profile/index.tsx
@@ -1,4 +1,10 @@
 import { Button } from '@mui/material';
+import { useState } from 'react';
+import {
+    usePlaidLink,
+    PlaidLinkOptions,
+    PlaidLinkOnSuccess,
+} from 'react-plaid-link';
 
 export default function Profile() {
     const onClick = () => {
@@ -6,17 +12,39 @@ export default function Profile() {
         fetch('/api/plaid', {
             method: 'POST',
         })
-            .then((res) => {
+            .then(async (res) => {
                 console.log(res);
+                const json = await res.json();
+                localStorage.setItem('link_token', json.link_token);
             })
             .catch((e) => {
                 console.log(e);
             });
     };
+    const [linkToken, setLinkToken] = useState('');
+    const getLinkToken = () => {
+        setLinkToken(localStorage.getItem('link_token') ?? '');
+    };
+    const { open, ready, error } = usePlaidLink({
+        token: linkToken,
+        onSuccess: (public_token, metadata) => {
+            console.log('public_token: ', public_token);
+            console.log('metadata: ', metadata);
+        },
+    });
 
     return (
-        <Button variant="outlined" onClick={onClick}>
-            Connect bank with plaid
-        </Button>
+        <>
+            <Button variant="outlined" onClick={onClick}>
+                Connect bank with plaid
+            </Button>
+            <Button variant="outlined" onClick={getLinkToken}>
+                Get Link token:
+            </Button>
+            <div>{linkToken}</div>
+            <Button variant="outlined" disabled={!ready} onClick={() => open()}>
+                Connect bank with plaid
+            </Button>
+        </>
     );
 }
